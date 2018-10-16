80f64c34ccb3169d56d941b9ed18d9a9ef90f122
mike <mikemayer67@vmwishes.com>
2018-10-16 14:36:47 -0400

    Added Xcode skeleton

---
+++ DLX-iOS/DLX-iOS/DLX_iOS.h
@@ -0,0 +1,19 @@
+//
+//  DLX_iOS.h
+//  DLX-iOS
+//
+//  Created by Mike Mayer on 10/16/18.
+//  Copyright © 2018 VMWishes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DLX_iOS.
+FOUNDATION_EXPORT double DLX_iOSVersionNumber;
+
+//! Project version string for DLX_iOS.
+FOUNDATION_EXPORT const unsigned char DLX_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DLX_iOS/PublicHeader.h>
+
+
